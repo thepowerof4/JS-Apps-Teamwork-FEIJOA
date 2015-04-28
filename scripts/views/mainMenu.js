c29dfe3dfe8253e2515028929269e81a4b9bfc9d
iliancho99 <iliqn.kordev@gmail.com>
2015-04-28 20:38:57 +0300

    Add userMenu

--- scripts/views/mainMenu.js
+++ scripts/views/mainMenu.js
@@ -1,5 +1,18 @@
 (function () {
     if(sessionStorage['logged-in']){
-        console.log(sessionStorage['logged-in']);
+        $("#mainMenuButtons").empty();
+        var user = JSON.parse(sessionStorage['logged-in']);
+        $("#mainMenuButtons").append($("<span>").text(user.username));
+    }else{
+        $("#mainMenuButtons").empty();
+
+        var homeBtn = $("<a href=\"#/\">Home |</a>");
+        var logBtn = $("<a href=\"#/login\">Login |</a>");
+        var regBtn = $("<a href=\"#/register\">Register</a>");
+
+        $("#mainMenuButtons")
+            .append(homeBtn)
+            .append(logBtn)
+            .append(regBtn);
     }
 }());
